bb7a740da8e0c3ea59abcaf2e80c369794bd0342
Fábio Kozlowski <cymonbr@gmail.com>
2017-10-29 03:47:29 -0200

    [Init] Add the initial version of the extension

---
+++ README.md
@@ -0,0 +1,40 @@
+# SQL Formatter
+
+**SQL Formatter** is an extension for formatting of _SQL_ files using _NodeJS_ component.
+
+In order for the extension to work correctly, it is necessary to have NodeJS installed, since one component will be used, being _sql-formatter_.
+
+# Features
+
+Performs _SQL Formatter_ to format your code using _NodeJS_ component.
+
+## Execution Commands
+
+First open the **Command Pallete** _(**Win:** Ctrl+Shift+P / **Mac:** Cmd+Shift+p)_ and type **Format**, then just select the command you want.
+* **Format Document**
+
+## Predefined Shortcuts
+
+* **Format Document** _(**Win:** Shift+Alt+F / **Mac:** Shift+Option+F)_
+
+# Requirements
+
+You need to have _NodeJS_ because the **SQL Formatter** component is used as extension dependencies.
+
+* **SQL Formatter** (https://github.com/TeamSQL/sql-formatter)
+
+# Release Notes
+
+First version of the extension, no important note at the moment **(")(^_^)(")**.
+
+>If you have ideas for implementations and improvements please contact me at _cymon@studiowoz.com.br_ or skype _cymonbr_.
+>
+>Also follow the evolution of the extension in [github](https://github.com/cymonbr/SqlFormatter-VSCode).
+
+### Version 1.0.0
+
+Initial version of the extension!!! Go go go
+
+# License
+
+MIT
---
+++ src/extension.ts
@@ -0,0 +1,63 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { window, TextEditor, Range, Position } from 'vscode';
+import * as sqlFormatter  from 'sql-formatter';
+
+var sqlChannel = vscode.window.createOutputChannel('sqlFormatter');
+var editor     = vscode.window.activeTextEditor;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "sql Formatter" is now active!');
+
+    let sqlFormatter  = new sqlFormatterClass();
+
+    vscode.languages.registerDocumentFormattingEditProvider('sql', {
+        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+            let data = sqlFormatter.formatter();
+            return [vscode.TextEdit.replace(new Range(data.start, data.end), data.text)];
+        }
+    });
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
+
+export class sqlFormatterClass {
+    constructor() {}
+
+    formatter() {
+        let text   = editor.document.getText();
+
+        sqlChannel.append("---------------------------\n");
+        sqlChannel.append("Iniciando formatação do SQL\n");
+        sqlChannel.append("-------------**------------\n");
+
+        if(text.trim()!='') {
+            let textFormatted = sqlFormatter.format(text, {
+                language: "sql", // Defaults to "sql"
+                indent: "    "   // Defaults to two spaces
+            });
+
+            const document = editor.document;
+            const lastLine = document.lineAt(document.lineCount-1);
+
+            const start    = new Position(0, 0);
+            const end      = new Position(document.lineCount-1, lastLine.text.length);
+
+            sqlChannel.append("Formatação concluída!\n");
+            sqlChannel.append("---------------------------\n\n");
+
+            return {"start": start, "end": end, "text": textFormatted};
+        } else {
+            sqlChannel.append("Não foi possível localizar o texto para formatação\n");
+            sqlChannel.append("---------------------------\n\n");
+
+            vscode.window.showErrorMessage('Não foi possível localizar o texto para formatação!');
+        }
+    }
+}
