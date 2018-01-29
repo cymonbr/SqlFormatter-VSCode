775aaa8ec79d3d8473c754a9e5ba64c5e067ece6
Fábio Kozlowski <fabio.kozlowski@webhub.com.br>
2018-01-29 11:39:13 -0200

    Remove "var editor" from the beginning of the file

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,6 @@ import { window, TextEditor, Range, Position } from 'vscode';
 import * as sqlFormatter  from 'sql-formatter';
 
 var sqlChannel = vscode.window.createOutputChannel('sqlFormatter');
-var editor     = vscode.window.activeTextEditor;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
